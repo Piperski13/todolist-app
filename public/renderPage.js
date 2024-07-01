fbbfb3876cf1768dd3ef3b3c58f1a7b233975602
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-07-01 21:59:31 +0200

    Version 3.0 (first stage of a app, todo cards rendering made)

--- public/renderPage.js
+++ public/renderPage.js
@@ -1,8 +1,40 @@
-import {clickEvents} from './scripts/clickEvent.js';
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
 
-
-function renderPage(){
-  clickEvents();
-  console.log('hello from renderPage.js');
-}
 renderPage();
+
+function renderPage() {
+  let todoListHTML = '';
+
+  todoList.forEach(element => {
+    const html = `
+      <div class="card"><h1>${element}</h1></div>
+    `;
+    todoListHTML += html;
+  });
+
+  document.querySelector('.js-render-page').innerHTML = todoListHTML;
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+// Event listener for add button
+document.getElementById('button-add').addEventListener('click', () => {
+  document.getElementById('overlay').style.display = 'flex';
+});
+
+// Event listener for close button
+document.getElementById('button-close').addEventListener('click', () => {
+  document.getElementById('overlay').style.display = 'none';
+});
+
+// Event listener for addTodoCard button
+document.getElementById('addButton').addEventListener('click', () => {
+  const inputElement = document.getElementById('popupInput');
+  const inputValue = inputElement.value;
+  if (inputValue) {
+    todoList.push(inputValue);
+    console.log(todoList);
+    inputElement.value = '';
+    document.getElementById('overlay').style.display = 'none';
+    renderPage();
+  }
+});
--- public/scripts/clickEvent.js
+++
@@ -1,9 +0,0 @@
-export function clickEvents(){
-  document.getElementById('button-add').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'flex';
-  });
-  
-  document.getElementById('button-close').addEventListener('click', function() {
-    document.getElementById('overlay').style.display = 'none';
-  });
-};
